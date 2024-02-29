47fc73f0034a04cad7570a4cf1021c95671eaba7
Abent Kirill <abent.kirill@outlook.com>
2024-02-29 20:31:27 +0400

    Weather Timer + refactoring

--- Assistant_Kira/CommandExecutor.cs
+++ Assistant_Kira/CommandExecutor.cs
@@ -6,36 +6,26 @@ using Telegram.Bot.Types;
 
 namespace Assistant_Kira;
 
-internal sealed class CommandExecutor : ICommandExecutor
+internal sealed class CommandExecutor(ILogger<CurrencyCommand> logger, IEnumerable<ICommand> commands, KiraBot kiraBot) : ICommandExecutor
 {
-	private readonly ILogger<CurrencyCommand> _logger;
-	private readonly IEnumerable<ICommand> _commands;
-	private readonly KiraBot _kiraBot;
-
-	public CommandExecutor(ILogger<CurrencyCommand> logger, IEnumerable<ICommand> commands, KiraBot kiraBot)
-	{
-		_logger = logger;
-		_commands = commands;
-		_kiraBot = kiraBot;
-	}
-
-	public async Task ExecuteAsync(Update update)
+    public async Task ExecuteAsync(Update update)
 	{
 		var text = update.Message!.Text;
 
 		if (string.IsNullOrEmpty(text))
 		{
-			return;
+            logger.LogWarning("Ввели пустую команду: {CommandText}", text);
+            return;
 		}
 		try
 		{
-			var command = _commands.SingleOrDefault(c => c.Name.Equals(text, StringComparison.OrdinalIgnoreCase))
+			var command = commands.SingleOrDefault(c => c.Name.Equals(text, StringComparison.OrdinalIgnoreCase))
 			?? throw new ArgumentException("Такой команды нет");
-			await _kiraBot.TelegramApi.SendTextMessageAsync(update.Message.Chat.Id, command.Execute(text.Split(' ')[1..]));
+			await kiraBot.TelegramApi.SendTextMessageAsync(update.Message.Chat.Id, command.Execute(text.Split(' ')[1..]));
 		}
 		catch (Exception ex)
 		{
-			_logger.LogError(ex, "Ошибка команды {text}", text);
+			logger.LogError(ex, "Ошибка команды {text}", text);
 			throw;
 		}
 	}
@@ -44,13 +34,13 @@ internal sealed class CommandExecutor : ICommandExecutor
     {
         try
         {
-            var command = _commands.SingleOrDefault(c => c.Name.Equals(text, StringComparison.OrdinalIgnoreCase))
+            var command = commands.SingleOrDefault(c => c.Name.Equals(text, StringComparison.OrdinalIgnoreCase))
             ?? throw new ArgumentException("Такой команды нет");
             return command.Execute(text.Split(' ')[1..]);
         }
         catch (Exception ex)
         {
-            _logger.LogError(ex, "Ошибка команды {text}", text);
+            logger.LogError(ex, "Ошибка команды {text}", text);
             throw;
         }
     }
---
+++ Assistant_Kira/Controllers/WebHookController.cs
@@ -0,0 +1,23 @@
+﻿using Assistant_Kira.Models;
+using Assistant_Kira.Models.Webhooks.GitLab;
+
+using Microsoft.AspNetCore.Mvc;
+
+using Telegram.Bot;
+
+namespace Assistant_Kira;
+
+[Route("webhook")]
+[ApiController]
+public sealed class WebHookController(KiraBot kiraBot) : ControllerBase
+{
+    [HttpPost]
+	public IActionResult GitLabWebHook([FromBody] WebhookMessage webhookMessage)
+	{
+		if (webhookMessage.Stages.Any(x => x.Status.Equals("failed", StringComparison.InvariantCultureIgnoreCase)))
+		{
+			kiraBot.TelegramApi.SendTextMessageAsync(1548307601, webhookMessage.ToString());
+		}
+		return Ok();
+	}
+}
