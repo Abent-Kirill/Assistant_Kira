139b50e30de1382cb477108bc9f3db0277dfbda8
musfobbie@mail.ru <musfobbie@mail.ru>
2024-03-09 11:54:02 +0300

    Добавление команд, связанных с новостями

---
+++ Assistant_Kira/Commands/NewsCommand.cs
@@ -0,0 +1,15 @@
+﻿
+using Assistant_Kira.Services;
+
+namespace Assistant_Kira.Commands
+{
+    internal class NewsCommand(LentaNewsService lentaNewsService) : ICommand
+    {
+        public string Name => "Новости";
+
+        public Task<string> ExecuteAsync(IEnumerable<string> args)
+        {
+            return Task.Run(lentaNewsService.GetCurrentNews);
+        }
+    }
+}
